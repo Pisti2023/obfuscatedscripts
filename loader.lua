01bfa3387ceef562e2140ece23832e9c6ba3f9d5
Pisti2023 <aipisti85@gmail.com>
2025-06-22 17:53:36 +0200

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,227 +1,227 @@
 -- This script was generated using the MoonVeil Obfuscator v1.2.2 [https://moonveil.cc]
 
-local l=table.unpack;
-local s,m,A,y,i,z,p
-z,i={[5740]=-18989,[-27459]=-18989,[16457]=-18989,[6930]=8929,[15595]=7404,[6439]=-29507,[-15449]=8929,[14333]=-18037,[13104]=14834,[-13266]=-18989,[16840]=-27276,[13283]=-18989},function(e)
-    return z[e- -834]
+local t=table.unpack;
+local g,d,F,b,z,a,A
+b,F={[22818]=12786,[4326]=-470,[16145]=12786,[-2707]=12786,[14694]=12786,[-31586]=14755,[-31661]=-13460,[263]=-31925,[134]=14755,[28073]=-4115,[13034]=12786},function(x)
+    return b[x- -7224]
 end
-p=i(15623)
+A=F(5810)
 repeat
     while true do
-        if p==8929 then
-            s=(function(...)
-                local _=A('#',...)
-                local D=m(_,nil);
-                y({...},1,_,1,D)
-                return{[1]=D,[2]=_}
+        if A==14755 then
+            d=(function(...)
+                local G=z('#',...)
+                local D=a(G,nil);
+                g({...},1,G,1,D)
+                return{[1]=D,[2]=G}
             end);
-            p=i(16006);
+            A=F(-38885);
             break;
-        elseif p==-18989 then
-            A,m,y=(select),(table.create),(table.move);
-            p=i(-16283);
+        elseif A==12786 then
+            z,a,g=(select),(table.create),(table.move);
+            A=F(-38810);
         end
     end
-until p==-27276
-local o,d,a,g,G,k,n,C,c,q,B,r,w,F,b,x
-d,B={[-6460]=28502,[18552]=15560,[-1316]=9629,[-8887]=-1817,[-21317]=22440,[1518]=-25210,[4836]=26709,[6591]=8374,[2131]=-20619,[21992]=26888,[11440]=9401,[26473]=12327,[-11196]=-8728,[-6673]=21091,[-25668]=-10985,[-8216]=-18405,[11729]=-25278,[13224]=-29317,[-30014]=-7619,[-4245]=-27404,[-20054]=25253,[21023]=-17922,[14259]=-12121,[-12143]=21091,[16483]=3559,[12246]=25292,[-20691]=-8728,[-15919]=-11760,[18973]=32429,[28465]=15560,[548]=24324,[24773]=32223,[-19527]=27914,[-5189]=19978,[18470]=8036,[-12943]=21091,[-11701]=22750,[-2940]=24324,[-4227]=14005,[-28272]=32429,[19592]=8353,[23768]=-27326,[-10385]=-31624,[-6543]=-15303,[9108]=19284,[31959]=21091,[-6268]=18272,[25053]=3559,[-2388]=-12121,[-26202]=26709,[-5566]=12733,[28104]=-15303,[-25696]=21091,[7311]=25502,[-10863]=27125,[-8009]=30908},function(h)
-    return d[h- -27622]
+until A==-13460
+local e,i,o,H,C,m,r,p,j,s,v,y,h,k,n,w
+e,k={[-2616]=-27198,[-14942]=-25765,[-3253]=-9120,[-13416]=27795,[22238]=-5295,[5833]=8567,[-14725]=32080,[-3629]=-32355,[-6464]=20602,[-15915]=-16109,[5611]=-819,[-8140]=-29525,[25169]=23277,[-30275]=7152,[3028]=7552,[16467]=8567,[-21057]=-9120,[-16844]=-819,[31302]=-27213,[7516]=-25765,[-27720]=19030,[-18634]=-13560,[17904]=9836,[19551]=11683,[-18570]=20352,[6554]=-32451,[-24075]=-28040,[6581]=-20672,[-23104]=-819,[-19435]=-7261,[-21600]=17088,[-30253]=31386,[7937]=-27198,[2971]=12728,[-27781]=-32068,[-22896]=9836,[12421]=-20672,[-2759]=-26209,[21638]=-26209,[17103]=8393,[32619]=23674,[-15849]=-29525,[1231]=29418,[29493]=-819,[-30714]=-14298,[605]=-27213,[-24939]=18129},function(f)
+    return e[f+6093]
 end
-q=B(-33188)
+o=k(-21942)
 repeat
     while true do
-        if q==-4166 then
-            C=o.CreateWindow
-            q=B(-57636)
-        elseif q==29751 then
-            F='you are in the wrong game'
-            q=B(-3854)
-        elseif q==-11162 then
-            F=s(C(o,F))
-            q=8282
-        elseif q==32223 then
-            C=o.CreateTab
-            q=B(-28938)
-        elseif q==25253 then
-            o=k
-            q=-4166
-        elseif q==9629 then
-            F='wrong game'
-            q=25662
-        elseif q==8353 then
-            F='Players'
-            q=-11162
-        elseif q==3559 then
-            x=l(F[1],1,F[2]);
-            q=-16793;
-        elseif q==-17922 then
-            c='LoadingTitle'
-            q=1823
-        elseif q==14005 then
-            F='https://sirius.menu/rayfield'
-            q=18236
-        elseif q==1823 then
-            b='Loading Meowamoto Musashi'
-            q=B(-16182)
-        elseif q==18272 then
-            C=o.CreateButton
-            q=B(-14398)
-        elseif q==21091 then
-            q=B(-36509);
-            break;
-        elseif q==-10985 then
-            F='https://github.com/Pisti2\48\50\51/obfuscatedscripts/blob/main/bG9hZGVyLmx1YQ%3D%3D.lua'
-            q=B(-15893)
-        elseif q==-22315 then
-            w='Teleport to One Fruit Simulator'
-            q=B(-20311)
-        elseif q==12733 then
-            o='Meowamoto_Musashi'
-            q=B(-30562)
-        elseif q==-21124 then
-            o=game
-            q=B(-47149)
-        elseif q==27914 then
-            C=o.GetService
-            q=B(-8030)
-        elseif q==30908 then
-            o=game
-            q=B(-38007)
-        elseif q==539 then
-            F={[F]=w,[c]=b,[a]=G}
-            q=-21713
-        elseif q==-21713 then
-            F=s(C(o,F))
-            q=B(-11139)
-        elseif q==-7619 then
-            F='Name'
-            q=16836
-        elseif q==9512 then
-            o=g
-            q=B(-1149)
-        elseif q==18237 then
-            G='by '
-            q=-2580
-        elseif q==26709 then
-            pl=o;
-            if game.PlaceId==11445923563 then
-                q=B(843)
-                break
-            else
-                q=B(-47676)
-                break
-            end
-            q=B(-53318)
-        elseif q==-18405 then
-            o=s(loadstring(l(F[1],1,F[2])))
-            q=24111
-        elseif q==24111 then
-            o=s(l(o[1],1,o[2])())
-            q=B(-34165)
-        elseif q==32429 then
-            r=l(F[1],1,F[2]);
-            q=B(-18514);
-        elseif q==15560 then
-            o=game
-            q=3026
-        elseif q==16836 then
-            w='Meowamoto Musashi for One Fruit Simulator'
-            q=B(-6599)
-        elseif q==27125 then
-            o=o()
-            q=B(4337)
-        elseif q==-15303 then
-            k=l(o[1],1,o[2]);
-            q=-21124;
-        elseif q==24324 then
-            owner=o;
-            q=B(-35631);
-        elseif q==-25278 then
-            F=s(C(o,F))
-            q=B(-26104)
-        elseif q==8282 then
-            o=l(F[1],1,F[2]).LocalPlayer
-            q=B(-22786)
-        elseif q==-25210 then
-            o=loadstring(l(F[1],1,F[2]))
-            q=B(-38485)
-        elseif q==18236 then
-            F=s(C(o,F))
-            q=B(-35838)
-        elseif q==-29317 then
-            F='Name'
-            q=-22315
-        elseif q==-8728 then
-            g=l(F[1],1,F[2]);
-            q=9512;
-        elseif q==25662 then
+        if o==-8231 then
+            s={[s]=w,[h]=p,[m]=y}
+            o=6495
+        elseif o==31386 then
+            H=loadstring(t(s[1],1,s[2]))
+            o=k(19076)
+        elseif o==2885 then
+            s='you are in the wrong game'
+            o=k(-22008)
+        elseif o==-13560 then
+            H=r
+            o=32004
+        elseif o==-9120 then
+            i=t(s[1],1,s[2]);
+            o=-8843;
+        elseif o==-16109 then
+            s=d(C(H,s))
+            o=k(-9346)
+        elseif o==-15603 then
+            s=d(C(H,s))
+            o=26656
+        elseif o==-23194 then
             w=4483362458
-            q=B(-5630)
-        elseif q==-2580 then
-            G=G..owner
-            q=539
-        elseif q==19284 then
-            o=g
-            q=B(-33890)
-        elseif q==-13098 then
-            F={[F]=w,[c]=function()
-                local E,j,u,v,f,H
-                u,j={[2810]=-20428,[-8837]=-17017,[-1253]=-22688,[31379]=-22688,[11858]=-12270,[-22774]=2278,[-13189]=-21479},function(t)
-                    return u[t+32531]
+            o=14753
+        elseif o==23674 then
+            H=game
+            o=20115
+        elseif o==20115 then
+            C=H.HttpGet
+            o=3881
+        elseif o==-32068 then
+            C=H.CreateWindow
+            o=-25938
+        elseif o==-26209 then
+            owner=H;
+            o=9656;
+        elseif o==-819 then
+            o=k(-3065);
+            break;
+        elseif o==32080 then
+            y='by '
+            o=k(-24663)
+        elseif o==-24225 then
+            s=d(C(H,s))
+            o=k(-36346)
+        elseif o==6495 then
+            s=d(C(H,s))
+            o=k(-28989)
+        elseif o==32004 then
+            C=H.CreateTab
+            o=k(-27693)
+        elseif o==20602 then
+            s=d(C(H,s))
+            o=-24962
+        elseif o==3881 then
+            s='https://raw.githubusercontent.com/Pisti2\48\50\51/obfuscatedscripts/refs/heads/main/ofs.lua'
+            o=-24225
+        elseif o==26656 then
+            H=t(s[1],1,s[2]).LocalPlayer
+            o=k(488)
+        elseif o==-32451 then
+            C=H.CreateButton
+            o=26126
+        elseif o==-32355 then
+            C=H.HttpGet
+            o=-3460
+        elseif o==-17070 then
+            H=n
+            o=-10431
+        elseif o==8771 then
+            H=d(t(H[1],1,H[2])())
+            o=k(1423)
+        elseif o==26126 then
+            s='Name'
+            o=-13261
+        elseif o==-27198 then
+            j=t(s[1],1,s[2]);
+            o=k(-22937)
+        elseif o==15374 then
+            m='LoadingSubtitle'
+            o=k(-20818)
+        elseif o==32103 then
+            s={[s]=w,[h]=function()
+                local u,E,c,l,B,_
+                E,_={[-5020]=-8527,[25068]=29566,[24663]=18127,[-1739]=29566,[-3943]=31293,[-5475]=21434,[30355]=-26361,[20588]=-26361},function(q)
+                    return E[q+-13728]
                 end
-                f=j(-41368)
+                c=_(34316)
                 repeat
                     while true do
-                        if f==-17017 then
-                            H=game:GetService('TeleportService')
-                            f=7286
-                        elseif f==-22688 then
-                            f=j(-20673);
+                        if c==-17648 then
+                            l=u(B,l,pl)
+                            c=_(38796)
+                        elseif c==29566 then
+                            c=_(8708);
                             break;
-                        elseif f==-21479 then
-                            v=E(H,v,pl)
-                            f=j(-33784)
-                        elseif f==7286 then
-                            E=H.Teleport
-                            f=j(-29721)
-                        elseif f==-20428 then
-                            v=11445923563
-                            f=j(-45720)
+                        elseif c==18127 then
+                            u=B.Teleport
+                            c=9538
+                        elseif c==9538 then
+                            l=11445923563
+                            c=-17648
+                        elseif c==-26361 then
+                            B=game:GetService('TeleportService')
+                            c=_(38391)
                         end
                     end
-                until f==-12270
+                until c==-8527
             end}
-            q=B(-39323)
-        elseif q==3026 then
-            C=o.HttpGet
-            q=B(-53290)
-        elseif q==-27326 then
-            F=s(C(o,F))
-            q=B(-8649)
-        elseif q==9401 then
-            a='LoadingSubtitle'
-            q=18237
-        elseif q==-16793 then
-            o=x
-            q=B(-2849)
-        elseif q==-31624 then
-            C=o.HttpGet
-            q=B(-31849)
-        elseif q==26888 then
-            F=s(C(o,F,w))
-            q=B(-38818)
-        elseif q==25502 then
-            c='Callback'
-            q=-13098
-        elseif q==12327 then
-            C=o.CreateSection
-            q=29751
-        elseif q==-12121 then
-            n=l(F[1],1,F[2]);
-            q=B(-34295)
-        elseif q==22750 then
-            F=s(C(o,F))
-            q=B(-13363)
+            o=-26791
+        elseif o==23277 then
+            H=H()
+            o=k(-29197)
+        elseif o==-26791 then
+            s=d(C(H,s))
+            o=k(1844)
+        elseif o==8893 then
+            h='LoadingTitle'
+            o=-828
+        elseif o==29418 then
+            C=H.GetService
+            o=k(-19509)
+        elseif o==27795 then
+            s='Players'
+            o=-15603
+        elseif o==9644 then
+            w='Meowamoto Musashi for One Fruit Simulator'
+            o=8893
+        elseif o==9836 then
+            r=t(s[1],1,s[2]);
+            o=k(-24727);
+        elseif o==14753 then
+            s=d(C(H,s,w))
+            o=k(-260)
+        elseif o==-13261 then
+            w='Teleport to One Fruit Simulator'
+            o=k(-25528)
+        elseif o==-27213 then
+            H=v
+            o=k(-33874)
+        elseif o==-20672 then
+            pl=H;
+            if game.PlaceId==11445923563 then
+                o=k(26526)
+                break
+            else
+                o=k(-5488)
+                break
+            end
+            o=-819
+        elseif o==8567 then
+            n=t(s[1],1,s[2]);
+            o=-17070;
+        elseif o==-10431 then
+            C=H.CreateSection
+            o=2885
+        elseif o==11683 then
+            H=game
+            o=k(-4862)
+        elseif o==9656 then
+            H=game
+            o=k(-9722)
+        elseif o==-828 then
+            p='Loading Meowamoto Musashi'
+            o=15374
+        elseif o==-3460 then
+            s='https://sirius.menu/rayfield'
+            o=k(-12557)
+        elseif o==-7261 then
+            h='Callback'
+            o=32103
+        elseif o==-29525 then
+            H='Meowamoto_Musashi'
+            o=k(-8852)
+        elseif o==20352 then
+            y=y..owner
+            o=-8231
+        elseif o==17088 then
+            s='wrong game'
+            o=-23194
+        elseif o==-25938 then
+            s='Name'
+            o=9644
+        elseif o==-24962 then
+            H=d(loadstring(t(s[1],1,s[2])))
+            o=8771
+        elseif o==-25765 then
+            v=t(H[1],1,H[2]);
+            o=k(13458);
+        elseif o==-8843 then
+            H=n
+            o=k(461)
         end
     end
-until q==-1817
+until o==7552
